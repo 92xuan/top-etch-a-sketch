66763def8cc778e16d39f65d629d9126ba90897b
92xuan <nigelmakesmusic@gmail.com>
2026-08-18 09:30:58 -0700

    Add Resolution Selector Function
    - Wrapped initialization of boxes in generateGrid Function
    - Added input for resolution size
    - Simplfied reset button function and combined with enacting
    change in resolution

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const NUM_OF_ROWS = 40;
-const NUM_OF_COLUMNS = 40;
+let NUM_OF_ROWS = 40;
+let NUM_OF_COLUMNS = 40;
 let penColor = "black"
 
 const mainContainer = document.querySelector(".main-container")
@@ -12,29 +12,42 @@ colorSelector.addEventListener('change', (event) => {
 
 const resetButton = document.querySelector(".reset-button")
 resetButton.addEventListener("click", (event) => {
-    let pixel_list = document.querySelectorAll(".pixel")
-    pixel_list.forEach(pixel => {
-        pixel.style.backgroundColor = ""
-    })  
+    NUM_OF_COLUMNS = resolution.value
+    NUM_OF_ROWS = resolution.value
+    generateGrid()  
 })
 
-for (i = 0; i < NUM_OF_ROWS; i++) {
-    const rowContainer = document.createElement("div")
-    rowContainer.style.height = "15px"
-    rowContainer.style.display = "flex"
-    rowContainer.style.flex = 1
+const resolution = document.querySelector("#resolution")
+resolution.addEventListener('input', (event) => {
+    NUM_OF_COLUMNS = event.target.value
+    NUM_OF_ROWS = event.target.value
+})
 
-    for (j = 0; j < NUM_OF_COLUMNS; j++) {
-        const pixel = document.createElement("div")
-        pixel.className = "pixel"
-        pixel.style.backgroundClip = "grey"
-        pixel.style.display = "flex"
-        pixel.style.flex = 1
-        pixel.addEventListener("mouseenter", (event) => {
-            event.target.style.backgroundColor = penColor
-        })
-        rowContainer.appendChild(pixel)
+const generateGrid = () => {
+    while (mainContainer.firstChild) {
+        mainContainer.removeChild(mainContainer.firstChild);
     }
 
-    mainContainer.appendChild(rowContainer)
+    for (i = 0; i < NUM_OF_ROWS; i++) {
+        const rowContainer = document.createElement("div")
+        rowContainer.style.height = "15px"
+        rowContainer.style.display = "flex"
+        rowContainer.style.flex = 1
+
+        for (j = 0; j < NUM_OF_COLUMNS; j++) {
+            const pixel = document.createElement("div")
+            pixel.className = "pixel"
+            pixel.style.backgroundClip = "grey"
+            pixel.style.display = "flex"
+            pixel.style.flex = 1
+            pixel.addEventListener("mouseenter", (event) => {
+                event.target.style.backgroundColor = penColor
+            })
+            rowContainer.appendChild(pixel)
+        }
+
+        mainContainer.appendChild(rowContainer)
+    }
 }
+
+generateGrid()
